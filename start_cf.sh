82441f4272c491ac9df5b91522ececbf5a758d7c
trbateman <t.r.bateman85@gmail.com>
2019-09-13 16:32:06 +0100

    Added envars for script`

--- start_cf.sh
+++ start_cf.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -xe
 
+ICMS_WEB_PORT="${ICMS_WEB_PORT:-8080}"
+ICMS_DEBUG="${ICMS_DEBUG:-False}"
+ICMS_MIGRATE="${ICMS_MIGRATE:-True}"
+ICMS_NUM_WORKERS="${ICMS_NUM_WORKERS:-3}"
+
 python manage.py migrate --noinput
 
 python manage.py collectstatic --noinput
