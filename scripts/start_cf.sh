b1e35185f1c586b531eb0d904738c46775c361bc
trbateman <t.r.bateman85@gmail.com>
2019-09-13 17:05:00 +0100

    Added some conditional

--- scripts/start_cf.sh
+++ scripts/start_cf.sh
@@ -7,9 +7,15 @@ ICMS_NUM_WORKERS="${ICMS_NUM_WORKERS:-3}"
 
 npm run deploy
 
-python manage.py migrate --noinput
+if [ "${ICMS_MIGRATE}" = 'True' ]; then
+  echo "Running migrations"
+  python manage.py migrate --noinput
+  python manage.py loaddata --app web web/fixtures/web/*.json
+fi
 
-python manage.py collectstatic --noinput
+if [ "$ICMS_DEBUG" = 'False' ]; then
+  python manage.py collectstatic --noinput --traceback
+fi
 
 gunicorn config.wsgi \
          --name icms \
