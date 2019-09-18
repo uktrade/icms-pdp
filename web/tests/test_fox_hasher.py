7dbe2fd254be2219e06f737e27348d538062d58d
Boris K <borissk@msn.com>
2019-09-18 14:38:12 +0100

    Fix test name

--- web/tests/test_fox_hasher.py
+++ web/tests/test_fox_hasher.py
@@ -1,7 +1,7 @@
 from web.auth.fox_hasher import FOXPBKDF2SHA1Hasher
 
 
-class Test:
-    def test_(self):
+class TestPasswordHasher:
+    def test_encoding(self):
         result = FOXPBKDF2SHA1Hasher().encode('P4$$word', '3333:3F47482FD712BF0A', 10000)
         assert result == 'fox_pbkdf2_sha1$10000$3333:3F47482FD712BF0A$4051D4D9A0C5AA6845BD9A03E348FB2B'
