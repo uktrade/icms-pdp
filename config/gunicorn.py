bb5981f5262b5aeae7ec4420bf80b15bc8980e0f
Kevin Etienne <kevin.etienne@digital.trade.gov.uk>
2020-08-14 16:23:13 +0100

    Use eventlet instead of gunicorn

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -1,5 +1,4 @@
 import os
-from psycogreen.gevent import patch_psycopg
 
 
 ICMS_WEB_PORT = os.environ.get("ICMS_WEB_PORT", 8080)
@@ -7,13 +6,8 @@ ICMS_WORKER_CONNECTIONS = int(os.environ.get("ICMS_WORKER_CONNECTIONS", 1000))
 
 
 bind = f"0:{ICMS_WEB_PORT}"
-worker_class = "gevent"
-worker_connections = ICMS_WORKER_CONNECTIONS
+worker_class = "eventlet"
+worker_connections = f"{ICMS_WORKER_CONNECTIONS}"
 accesslog = "-"
 errorlog = "-"
 proc_name = "icms"
-
-
-def post_fork(server, worker):
-    patch_psycopg()
-    worker.log.info("Enabled async Psycopg2")
