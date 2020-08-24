3a9eb20eabdfd0f843fc7c1717e26e8fe00ef614
Kevin Etienne <kevin.etienne@digital.trade.gov.uk>
2020-08-24 16:14:58 +0100

    Rename hmrc to companieshouse
    Nothing related with hmrc

--- web/tests/company/test_companieshouse.py
+++ web/tests/company/test_companieshouse.py
@@ -2,17 +2,17 @@ from unittest.mock import Mock, patch
 
 import pytest
 
-from web.company.hmrc import CompaniesHouseException, api
+from web.company.companieshouse import CompaniesHouseException, api
 
 
-@patch("web.company.hmrc.requests.get")
+@patch("web.company.companieshouse.requests.get")
 def test_api_raise_error(_):
     """Raise an error when status code is not 200."""
     with pytest.raises(CompaniesHouseException):
         api("hello")
 
 
-@patch("web.company.hmrc.requests.get")
+@patch("web.company.companieshouse.requests.get")
 def test_api_ok(mock_get):
     response = Mock()
     response.status_code = 200
