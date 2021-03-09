73d88aa61fc102595dfcf79794b29fedb4d78995
Tom Bateman <tom.bateman@digital.trade.gov.uk>
2021-03-09 14:37:11 +0000

    ICMSLST-525 Updated form

--- run-tests.sh
+++ run-tests.sh
@@ -5,4 +5,5 @@ set -e
 export DJANGO_SETTINGS_MODULE=config.settings.test
 export ICMS_DEBUG=False
 
-docker-compose run --rm web pytest --tb=short --cov=web --cov=config --cov-report xml:test-reports/cov.xml --dist=loadfile --tx=4*popen "$@"
+#docker-compose run --rm web pytest --tb=short --cov=web --cov=config --cov-report xml:test-reports/cov.xml --dist=loadfile --tx=4*popen "$@"
+docker-compose run --rm web pytest --tb=short --cov=web --cov=config --cov-report html:test-reports/cov.html --dist=loadfile --tx=4*popen "$@"
