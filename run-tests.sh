027496659cfa5180aea6503bd8cab72195347c33
Tom Bateman <tom.bateman@digital.trade.gov.uk>
2021-03-09 16:37:27 +0000

    Removed extra line

--- run-tests.sh
+++ run-tests.sh
@@ -6,4 +6,3 @@ export DJANGO_SETTINGS_MODULE=config.settings.test
 export ICMS_DEBUG=False
 
 docker-compose run --rm web pytest --tb=short --cov=web --cov=config --cov-report xml:test-reports/cov.xml --dist=loadfile --tx=4*popen "$@"
-
